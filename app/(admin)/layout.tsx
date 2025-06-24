796d983587634a325f110f31184d7fd371b5d360
rashaan <rmng.developer@gmail.com>
2025-06-24 13:08:41 -0400

    Created Log in page

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,26 +1,33 @@
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-function AdminLayout({
+async function AdminLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return redirect(`/login`);
+  }
+
   return (
-      <div className="flex flex-col flex-1">
-           <Header />
-       <div className="flex flex-col flex-1 md:flex-row bg-gray-100">
+    <div className="flex flex-col flex-1">
+      <Header />
+      <div className="flex flex-col flex-1 md:flex-row bg-gray-100">
         {/* Sidebar */}
-           <Sidebar />
+        <Sidebar />
 
-            <div className="flex-1 flex justify-center lg:justify-start items-start max-w-5xl mx-auto w-full">
-                {/* Sidebar content */}
+        <div className="flex-1 flex justify-center lg:justify-start items-start max-w-5xl mx-auto w-full">
+          {/* Sidebar content */}
 
-                {children}
-            </div>
-
-       </div>
+          {children}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-export default AdminLayout
+export default AdminLayout;
