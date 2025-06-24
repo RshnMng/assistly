79cebfe217b4e047fc8dc3712a4776fed0b9ddf9
rashaan <rmng.developer@gmail.com>
2025-06-24 10:31:53 -0400

    adjusting chat display

---
+++ app/(admin)/components/Messages.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { Message } from "@/types/types";
+import { usePathname } from "next/navigation";
+import Avatar from "./Avatar";
+import { UserCircle } from "lucide-react";
+
+function Messages({
+  messages,
+  chatbotName,
+  chatSessionId,
+}: {
+  messages: Message[];
+  chatbotName: string;
+  chatSessionId: number;
+}) {
+  const path = usePathname();
+  const isReviewsPage = path.includes("/review-sessions");
+  return (
+    <div>
+      {messages.map((message) => {
+        const isSender = message.sender !== "user";
+
+        return (
+          <div
+            key={message.id}
+            className={`${
+              isSender ? "bg-green-500 " : "bg-orange-500 justify-end"
+            } relative flex m-6 p-2`}
+          >
+            {isReviewsPage && (
+              <p className="absolute -bottom-5 text-xs text-purple-800">
+                {" "}
+                sent: {new Date(message.created_at).toLocaleString()}
+              </p>
+            )}
+            <div>
+              {isSender ? (
+                <div className="flex">
+                  <Avatar
+                    seed={chatbotName}
+                    className="h-12 w-12 bg-white rounded-full border-2 border-[#2991EE]"
+                  />
+                  <p className="pt-3 ml-2">whats good?</p>
+                </div>
+              ) : (
+                <div className="flex">
+                  <p>yooo</p>
+                  <UserCircle className="text-[2991EE] ml-3" />
+                </div>
+              )}
+            </div>
+
+            {/* <p
+              className={`chat-bubble 
+                    ${
+                      isSender
+                        ? "chat-bubble-primary bg-[#4D7DFB] text-white"
+                        : "chat-bubble-secondary text-red-700"
+                    }`}
+            >
+              whats goodie?
+            </p> */}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+export default Messages;
--- app/(admin)/review-sessions/[id]/page.tsx
+++ app/(admin)/review-sessions/[id]/page.tsx
@@ -2,6 +2,7 @@ import { serverClient } from "@/lib/server/server-client";
 import { GET_CHAT_SESSION_MESSAGES } from "@/graphql/queries/queries";
 import { GetChatSessionMessagesVariables } from "@/types/types";
 import { GetChatSessionMessagesResponse } from "@/types/types";
+import Messages from "../../components/Messages";
 
 export const dynamic = "force-dynamic";
 
@@ -31,12 +32,24 @@ async function reviewOneSession({
   console.log("sane check 1", messages);
 
   return (
-    <div>
+    <div className="flex-1 p-10 pb-24">
       <h1 className="text-xl lg:text-3xl font-semibold">Session Review</h1>
 
-      <p className="font-light text-xs text-gray-400 mt-2">
+      <p className="font-light text-xs text-purple-600 mt-2">
         Started at {new Date(created_at).toLocaleString()}{" "}
       </p>
+
+      <h2 className="font-light mt-2">
+        Between {name} & {guestName} ({email})
+      </h2>
+
+      <hr className="my-10" />
+
+      <Messages
+        messages={messages}
+        chatSessionId={chatSessionId}
+        chatbotName={name}
+      />
     </div>
   );
 }
