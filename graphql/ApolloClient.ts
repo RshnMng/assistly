380846cb045b11c8f537b7932a9c7c7738aaf6ce
rashaan <rmng.developer@gmail.com>
2025-06-27 10:09:04 -0400

    fixed baseurl pointing to graphql

--- graphql/ApolloClient.ts
+++ graphql/ApolloClient.ts
@@ -13,7 +13,7 @@ export const BASE_URL =
     : "http://localhost:3000";
 
 const httpLink = createHttpLink({
-  uri: `${BASE_URL}/graphql`, // point to the new api route
+  uri: `${BASE_URL}/api/graphql`, // point to the new api route
   headers: {
     Authorization:
       "apikey bumate::local.net+1000::ea93f403d148d87f3fcd8342c6fb6ab113b528a06b40a8af5292f80b136fccb2",
